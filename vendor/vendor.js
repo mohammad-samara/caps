c1b376530ba6395f26f899a1885745127eb244af
mohammad-samara <mohammad.ab.samara@gmail.com>
2020-09-29 15:50:19 +0300

    in progress

--- vendor/vendor.js
+++ vendor/vendor.js
@@ -6,20 +6,19 @@ const storeName = process.env.storeName || 'Samara Store';
 const io = require('socket.io-client');
 const socket = io.connect('http://localhost:3000/caps');
 
-socket.emit('join',STORE_NAME);
+socket.emit('join',storeName);
 
 socket.on('delivered', payload=>{
-  console.log(`Thank you for delivering ${payload.orderId}`)
-})
+  console.log(`Thank you for delivering ${payload.orderId}`);
+});
 
 
 setInterval(()=>{
   let order = {
-    storeName: STORE_NAME,
+    storeName: storeName,
     orderId: faker.random.uuid(),
     customerName: faker.name.firstName(),
     address: faker.address.streetAddress(),
   };
-  socket.emit('pickup', order)
-},5000)
-
+  socket.emit('pickup', order);
+},5000);
